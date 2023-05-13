d1053216e0d0c774327aaac8dbf37084e8b52483
Yaroslav <ydomozhilov111@gmail.com>
2023-05-13 11:07:06 +0300

    3

--- Desktop/React-tutorial/my-app/src/App.js
+++ Desktop/React-tutorial/my-app/src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p className="title">Регистрация Кентов</p>
+      <p className="title">Регистрация.</p>
   
       <form className="App">
         Имя
